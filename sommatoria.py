0de2eab84a082b6e39e87f64d6da3e63323b0abf
carlo cifarelli <>
2022-11-15 13:10:41 +0000

    sommatoria

---
+++ sommatoria.py
@@ -0,0 +1,9 @@
+def sum_list(lista):
+    risultato=0
+    if lista==[]:
+       return None
+    for item in lista:
+        risultato=risultato+item
+    return risultato
+my_list=[]
+print("sommatoria vale:{}".format(sum(my_list)))
