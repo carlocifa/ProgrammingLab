ab6653d7c17cbcaa1b249eef404bf8a725b8e9f1
carlo cifarelli <115221087+carlocifa@users.noreply.github.com>
2022-11-22 16:37:15 +0000

    lezione 3

--- lezione3.py
+++ lezione3.py
@@ -1,15 +1,15 @@
 def sum_csv(file_name):
     values=[]
+    prezzo=0
+    for item in values:
+        prezzo=prezzo + item
+    return prezzo
     file_name=open('shampoo_sales.csv', 'r')
     for line in file_name:
         elements=line.split(',')
         if elements[0]!='Date':
             date=elements[0]
             value=elements[1]
-            values.append(value)
-            file_name.close()
-    prezzo=0
-    for item in values:
-        prezzo=prezzo + item
-    return prezzo
+    values.append(value)
+    file_name.close()
 sum_csv('file_name')
