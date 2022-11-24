872277a55a10b9abeef319a11bf05876b25909c1
carlo cifarelli <115221087+carlocifa@users.noreply.github.com>
2022-11-24 13:40:18 +0000

    lezione 3

--- lezione3.py
+++ lezione3.py
@@ -7,8 +7,5 @@ def sum_csv(nome_file):
             value=elements[1]
             values.append(float(value))
     file.close()
-    if(len(values)==0):
-        return None
-    else:
-        return sum(values)
+    
 
